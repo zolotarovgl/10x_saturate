19137e625ce52c96a70890c306ec78c211b08624
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2024-12-05 16:38:38 +0100

    added script location

--- saturation_table.py
+++ saturation_table.py
@@ -14,7 +14,6 @@ parser.add_argument('-s', '--nsteps', type=int, required=False,default = 10, hel
 parser.add_argument('-c', '--ncpu', type=int, required=False,default = 1, help='Number of CPUs to use')
 parser.add_argument('-t', '--temp', required=False,default = '_tmp', help='Temporary directory')
 parser.add_argument('-f', '--tabfile', required=False,default = 'tags.tab', help='File to store tags')
-
 args = parser.parse_args()
 
 BAM = args.bam
@@ -27,11 +26,15 @@ TEMP = args.temp
 TABFILE = args.tabfile
 
 
+CODE_DIR = os.path.dirname(os.path.abspath(__file__))
+gettags_script = os.path.join(CODE_DIR,'scripts/gettags.py')
+compute_saturation_script = os.path.join(CODE_DIR,'scripts/compute_saturation.r')
+
+
 # Remove temporary directory if it exists
 if os.path.exists(TEMP):
     print(f'Found temp directory {TEMP}! Remove it or set a new TEMP directory!')
     quit()
-    #subprocess.run(['rm', '-rf', TEMP])
 else:
     print(f'Creating temp directory {TEMP}')
     os.mkdir(TEMP)
@@ -40,7 +43,7 @@ print(f"Compute sequencing saturation curve")
 print(f"Alignment: {BAM}\nOutput: {OUTPUT}\nN cells: {NCELLS}\nMapping rate: {MAPPING_RATE}\nN threads: {NCPU}")
 
 
-gettags_command = ['python', 'scripts/gettags.py',  BAM, TEMP, '--num_threads', str(NCPU)]
+gettags_command = ['python', gettags_script,  BAM, TEMP, '--num_threads', str(NCPU)]
 print(f"Executing command: {' '.join(gettags_command)}")
 subprocess.run(gettags_command)
 
@@ -57,7 +60,7 @@ subprocess.run(['rm', '-rf', TEMP])
 
 print(f"Removed temp directory: {TEMP}")
 # Execute compute_saturation.r script
-subprocess.run(['Rscript', 'scripts/compute_saturation.r', '--ncpu', str(NCPU), '--mapping_rate', str(MAPPING_RATE), TABFILE, OUTPUT, str(NCELLS), '--nstep', str(NSTEPS)])
+subprocess.run(['Rscript', compute_saturation_script, '--ncpu', str(NCPU), '--mapping_rate', str(MAPPING_RATE), TABFILE, OUTPUT, str(NCELLS), '--nstep', str(NSTEPS)])
 
 # Uncomment the line below if you want to remove TABFILE after execution
 # subprocess.run(['rm', TABFILE])
